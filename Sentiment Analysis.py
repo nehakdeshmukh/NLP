2b81ec6908445e6b67f6f345527484bd42683737
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-28 13:09:08 -0300

    added lstm network

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -38,3 +38,14 @@ tokenizer.fit_on_texts(data['text'].values)
 X = tokenizer.texts_to_sequences(data['text'].values)
 X = pad_sequences(X)
 
+embed_dim = 128
+lstm_out = 196
+
+model = Sequential()
+model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+print(model.summary())
+
