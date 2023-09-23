d8cca8c07119c5e5f9822ef3db72335db347f5da
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-09-22 22:26:15 -0300

    added TensorFlow model

--- ChatBot/Chatbot.py
+++ ChatBot/Chatbot.py
@@ -81,4 +81,32 @@ except:
     with open("data.pickle","wb") as f:
         pickle.dump((words, labels, training, output), f)
     
+# tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None,len(training[0])])            
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)            
+    model.save("model.tflearn")
     
+    
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+                
+    return np.array(bag)
