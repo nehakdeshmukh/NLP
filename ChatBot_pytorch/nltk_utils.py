5af2f0f0cde4837acc1fc39337d60e818a87d6cf
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-09-23 06:17:58 -0300

    added nltk utils

---
+++ ChatBot_pytorch/nltk_utils.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 14:14:36 2023
+
+@author: nehak
+"""
+
+import nltk
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+import json
+import numpy as np
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+
+def bag_of_words(tokenized_sentence, words):
+    
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words:
+            bag[idx] = 1
+            
+    return bag
+
