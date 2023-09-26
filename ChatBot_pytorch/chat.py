eafca38a285b69f304e8125df1bc3f4fd6ec0ac0
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-09-26 17:07:31 -0300

    added chat.py

---
+++ ChatBot_pytorch/chat.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 17:01:52 2023
+
+@author: nehak
+"""
+
+import random
+import json
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+with open(r"intents.json", "r") as j_data:
+    intents = json.load(j_data)
+    
+    
+FILE =r"data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
