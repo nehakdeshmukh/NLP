0cde887175f6941d77adc7a93fafab0359d36106
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-10-03 21:49:36 -0300

    removed invalid categories

--- Sentiment analysis using BERT/sentiment_analysis_BERT.py
+++ Sentiment analysis using BERT/sentiment_analysis_BERT.py
@@ -7,5 +7,9 @@ Created on Tue Oct  3 21:42:17 2023
 
 import pandas as pd 
 
-df = pd.read_csv('Data/smile-annotations-final.csv', names=['id', 'text', 'category'])
+df = pd.read_csv('smile-annotations-final.csv', names=['id', 'text', 'category'])
 df.set_index('id', inplace=True)
+
+df.head()
+
+df["category"].value_counts()
