e8cd98cac662d5037eea47574ed5a73b055b294d
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-10-09 11:10:13 -0300

    added accuracy per class function

--- Sentiment analysis using BERT/sentiment_analysis_BERT.py
+++ Sentiment analysis using BERT/sentiment_analysis_BERT.py
@@ -130,4 +130,14 @@ def f1_score_func(preds, labels):
     return f1_score(labels_flat, preds_flat, average='weighted')
 
 
+def accuracy_per_class(preds, labels):
+    label_dict_inverse = {v: k for k, v in label_dict.items()}
+    
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
 
+    for label in np.unique(labels_flat):
+        y_preds = preds_flat[labels_flat==label]
+        y_true = labels_flat[labels_flat==label]
+        print(f'Class: {label_dict_inverse[label]}')
+        print(f'Accuracy: {len(y_preds[y_preds==label])}/{len(y_true)}\n')
