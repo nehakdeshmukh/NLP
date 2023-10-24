03914733160f67ccb6d3faa7b74d05f7a479b028
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-10-24 20:27:08 -0300

    refactor vectorizer

--- TopicModelingLDANMF/LDA_NMF.py
+++ TopicModelingLDANMF/LDA_NMF.py
@@ -181,4 +181,5 @@ def closest_doc_name(sentence, docs):
         cos.append(cosine_sim(', '.join(sentence.split(' ')),', '.join(docs[i])))
     return [titles[x] for x in np.argsort(cos)[-10:][::-1]]
 
-tfidf = TfidfVectorizer(tokenizer=words, stop_words=stop_words).fit(words_list)
+tfidf = TfidfVectorizer(stop_words=stop_words).fit(words_list)
+
