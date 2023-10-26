8412384f4c608c63928fedff59ea8003ac80885c
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-10-25 22:35:17 -0300

    added Speech recognition project

---
+++ Speech Emotion Detection/Speech emotion detection.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 18:12:19 2023
+
+@author: nehak
+"""
+
+import pandas as pd
+import numpy as np
+
+import os
+import sys
+
+Ravdess  = "Ravdess/audio_speech_actors_01-24/"
+Savee = "ALL/"
+
+
+
+        
