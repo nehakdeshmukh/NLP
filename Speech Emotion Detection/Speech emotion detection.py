abd21e668310612bfeb2c79ef3b67774544f3fe2
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-10-26 15:04:52 -0300

    changed integers to emotion

--- Speech Emotion Detection/Speech emotion detection.py
+++ Speech Emotion Detection/Speech emotion detection.py
@@ -11,8 +11,8 @@ import numpy as np
 import os
 import sys
 
-Ravdess  = "Ravdess/audio_speech_actors_01-24/"
-Savee = "ALL/"
+Ravdess  = r"C:\Neha\kaggle Projects\Ravdess\audio_speech_actors_01-24/"
+Savee = r"C:\Neha\kaggle Projects\Savee\ALL/"
 
 
 ravdess_directory_list = os.listdir(Ravdess)
@@ -36,3 +36,11 @@ emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
 
 path_df = pd.DataFrame(file_path, columns=['Path'])
 Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
+
+
+# changing integers to actual emotions.
+Ravdess_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 
+                             5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, 
+                            inplace=True)
+Ravdess_df.head()
+
