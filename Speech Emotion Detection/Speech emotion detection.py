83b9069999d34653c8db7b7c842106270e10b7d3
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-10-28 21:32:40 -0300

    added spectrogram function

--- Speech Emotion Detection/Speech emotion detection.py
+++ Speech Emotion Detection/Speech emotion detection.py
@@ -92,8 +92,18 @@ sns.despine(top=True, right=True, left=False, bottom=False)
 plt.show()
 
 
-def create_waveplot(data, sr, e):
+def waveplot(data, sr, e):
     plt.figure(figsize=(10, 3))
     plt.title('Waveplot for audio with {} emotion'.format(e), size=15)
     librosa.display.waveplot(data, sr=sr)
     plt.show()
+    
+    
+def spectrogram(data, sr, e):
+    X = librosa.stft(data)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(12, 3))
+    plt.title('Spectrogram for audio with {} emotion'.format(e), size=15)
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')   
+    plt.colorbar()
+    
