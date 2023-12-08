844b727539d06e9ace74c30d46a826739e0a393a
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-12-07 20:40:37 -0400

    added while loop

--- Image captioning using LSTM/Image Captioning.py
+++ Image captioning using LSTM/Image Captioning.py
@@ -132,27 +132,30 @@ def data_generator(data_keys, features, mapping, tokenizer, max_length, vocab_si
     # loop over images
     X1, X2, y = list(), list(), list()
     n = 0
-    for key in data_keys:
-        n += 1
-        captions = mapping[key]
-        
-        for caption in captions:
-           
-            seq = tokenizer.texts_to_sequences([caption])[0]
-            for i in range(1, len(seq)):
-                # split into input and output pairs
-                in_seq, out_seq = seq[:i], seq[i]
-                # pad input sequence
-                in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
-                
-                out_seq = to_categorical([out_seq],num_classes=vocab_size)[0]
-                
-                X1.append(features[key][0])
-                X2.append(in_seq)
-                y.append(out_seq)
-                
-                if n == batch_size:
-                    X1, X2, y = np.array(X1), np.array(X2), np.array(y)
-                    yield [X1, X2], y
-                    X1, X2, y = list(), list(), list()
-                    n = 0
+    while 1:
+        for key in data_keys:
+            n += 1
+            captions = mapping[key]
+            
+            for caption in captions:
+               
+                seq = tokenizer.texts_to_sequences([caption])[0]
+                for i in range(1, len(seq)):
+                    # split into input and output pairs
+                    in_seq, out_seq = seq[:i], seq[i]
+                    # pad input sequence
+                    in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                    
+                    out_seq = to_categorical([out_seq],num_classes=vocab_size)[0]
+                    
+                    X1.append(features[key][0])
+                    X2.append(in_seq)
+                    y.append(out_seq)
+                    
+                    if n == batch_size:
+                        X1, X2, y = np.array(X1), np.array(X2), np.array(y)
+                        yield [X1, X2], y
+                        X1, X2, y = list(), list(), list()
+                        n = 0
+                        
+                        
