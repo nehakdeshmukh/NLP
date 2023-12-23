cd11fd01823e5629c56e975f4c1f7bd1be52ffc1
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-12-23 07:23:54 -0400

    added script to remove punctuation

--- keyphrase extractionusing summarization/data/benchmark_data/clean_duplicate_papers.py
+++ keyphrase extractionusing summarization/data/benchmark_data/clean_duplicate_papers.py
@@ -20,3 +20,13 @@ file_kp20k_test = '..\\kp20k_testing.json'  # TEST data
 file_nus = 'NUS.json'  # TEST data 
 file_acm = 'ACM.json'  # TEST data 
 file_semeval = 'semeval_2010.json'  # TEST data 
+
+punctuation = string.punctuation + '\t' + '\n'
+table = str.maketrans(punctuation, ' '*len(punctuation))  # OR {key: None for key in string.punctuation}
+
+print(punctuation, 'LEN:', len(punctuation))
+
+
+def remove_punct(text):
+    clean_text = text.translate(table)
+    return clean_text
