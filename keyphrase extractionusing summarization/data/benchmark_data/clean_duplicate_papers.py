27de77b68826483eff408688e97eb6162e06da47
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-12-23 07:22:46 -0400

    created clean papers script

---
+++ keyphrase extractionusing summarization/data/benchmark_data/clean_duplicate_papers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 23 07:18:01 2023
+
+@author: nehak
+"""
+
+import json
+import string
+from pandas import json_normalize
+
+from tqdm import tqdm
+tqdm.pandas()
+
+
+file_kp20k = '..\\kp20k_training.json'  # TRAIN data
+file_kp20k_val = '..\\kp20k_validation.json'  # VALIDATION data
+file_kp20k_test = '..\\kp20k_testing.json'  # TEST data 
+
+file_nus = 'NUS.json'  # TEST data 
+file_acm = 'ACM.json'  # TEST data 
+file_semeval = 'semeval_2010.json'  # TEST data 
