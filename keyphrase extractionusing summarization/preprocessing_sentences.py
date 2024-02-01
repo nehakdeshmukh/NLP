9ca777cd9b189ea78de0885fe4ab9bce9c88af85
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2024-02-01 09:30:14 -0400

    removed punctuation

--- keyphrase extractionusing summarization/preprocessing_sentences.py
+++ keyphrase extractionusing summarization/preprocessing_sentences.py
@@ -221,3 +221,10 @@ def keyword_remove_punct_and_non_ascii(text):
     # remove all single letter except from 'a' and 'A'
     clean_text = [re.sub(r"\b[b-zB-Z]\b", "", keyw.translate(table).encode("ascii", "ignore").decode()) for keyw in text]  # remove non-ascii characters
     return clean_text
+
+
+data['abstract'] = data['abstract'].apply(remove_punct_and_non_ascii)
+data['keyword'] = data['keyword'].apply(keyword_remove_punct_and_non_ascii)
+print(data['keyword'])
+
+
