f6e08c6ce1a80f1b2f9eca46d6a3cdde659e7249
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2024-02-26 20:34:40 -0400

    added path for semeval sentences abstract

--- keyphrase extractionusing summarization/bi_lstm_crf.py
+++ keyphrase extractionusing summarization/bi_lstm_crf.py
@@ -154,4 +154,10 @@ elif args.select_test_set=="acm_sentences_abstract":
     x_filename = 'data\\preprocessed_data\\sentence_abstract\\x_ACM_SENTENC_ABSTRACT_preprocessed_TEXT'
     y_filename = 'data\\preprocessed_data\\sentence_abstract\\y_ACM_SENTENC_ABSTRACT_preprocessed_TEXT'
 
+elif args.select_test_set=="semeval_sentences_abstract":
+    test_data_size = 1979
+    x_test_filename = 'data\\preprocessed_data\\sentence_abstract\\x_SEMEVAL_SENTEC_ABSTRACT_TEST_data_preprocessed.hdf'
+    y_test_filename = 'data\\preprocessed_data\\sentence_abstract\\y_SEMEVAL_SENTEC_ABSTRACT_TEST_data_preprocessed'
+    x_filename = 'data\\preprocessed_data\\sentence_abstract\\x_SEMEVAL_SENTEC_ABSTRACT_preprocessed_TEXT'
+    y_filename = 'data\\preprocessed_data\\sentence_abstract\\y_SEMEVAL_SENTEC_ABSTRACT_preprocessed_TEXT'
 
